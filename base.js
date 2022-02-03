3e23ef0cd99fcf406948ff6aadf7cf5f8427a4df
Adriano Elias <aelias1995@gmail.com>
2022-02-02 19:22:36 -0500

    Added Word Picker Function
    readFromTxtFile() reads data from the word list fileTest.txt and selects one word to display. Instead of displaying the word will be stored and used for guess

--- base.js
+++ base.js
@@ -1,3 +1,20 @@
+//Global Variable where word will be stored
+let wordToGuess = "";
+//test function
 function test(){
     document.getElementById("demo").innerHTML = "Hello JavaScript";
+}
+//Function that reads what word to guess
+async function readFromTxTFile(){
+    //reads text file
+    const response = await fetch('fileTest.txt');
+    //convert to string
+    var data = await response.text();
+    const splitData = data.toLowerCase.split("\n");
+    wordToGuess = splitData[randVal(splitData.length)];
+    document.getElementById("demo").innerHTML = wordToGuess;
+}
+//Function returns random number based on length of list
+function randVal(lengthVal){
+    return Math.floor(Math.random() * lengthVal)
 }
